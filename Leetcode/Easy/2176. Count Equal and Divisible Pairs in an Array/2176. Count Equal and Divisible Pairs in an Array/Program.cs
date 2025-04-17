1f8ca92dd95b0202fa4e2decdac68b03273335d1
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-04-17 05:56:53 +0200

    two problems

---
+++ Leetcode/Easy/2176. Count Equal and Divisible Pairs in an Array/2176. Count Equal and Divisible Pairs in an Array/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _2176._Count_Equal_and_Divisible_Pairs_in_an_Array
+{
+
+    public class Solution
+    {
+
+        // time  : O(n*n)
+        // space : O(1)
+
+
+        public int CountPairs(int[] nums, int k)
+        {
+            if (nums == null || k == 0 || nums.Length ==1) return 0;
+            
+            int count = 0;
+            for (int i = 0; i < nums.Length -  1; i++)
+            {
+                for (global::System.Int32 j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[i] == nums[j] && i * j % k == 0 )
+                    {
+                        count++;
+                    }
+                }
+            }
+            return count;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine( s.CountPairs([3,1,2,2,2,1,3], 2));
+        }
+    }
+}
---
+++ Leetcode/Medium/2537. Count the Number of Good Subarrays/2537. Count the Number of Good Subarrays/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+
+namespace _2537._Count_the_Number_of_Good_Subarrays
+{
+    public class Solution
+    {
+        public long CountGood(int[] nums, int k)
+        {
+            bool status = true;
+            if (nums.Length == 1) return 0;
+            int number_of_arrays = 0;
+            Hashtable hashs = new Hashtable();
+            int all = 0;
+            int start = 0;
+            int end = 0;
+            while (end < nums.Length  ) 
+            { 
+
+                if (hashs[nums[end]] == null)
+                {
+                    hashs[nums[end]] = 1;
+                    end++;
+                    continue;
+                }
+                else
+                {
+                    if (status)
+                    {
+                        hashs[nums[end]] = (int)hashs[nums[end]] + 1;
+
+                        all = all + (int)hashs[nums[end]] - 1;
+
+
+                    }
+                    if (all< k)
+                    {
+
+                        end++;
+                        status = true;
+                    }
+                    else
+                    {
+                        number_of_arrays += nums.Length - end;
+
+                        all -= ((int)hashs[nums[start]] - 1);
+                        hashs[nums[start]] = (int)hashs[nums[start]] - 1;
+                        start++;
+                        status = false;
+                    }
+                }
+            }
+
+
+            return number_of_arrays  ;
+
+
+        }
+        
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(   s.CountGood([3, 1, 4, 3, 2, 2, 4], 2));
+        }
+    }
+}
