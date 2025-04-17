0c3601aea38f557a266b64e4bc30d9faaad75db3
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-04-17 18:10:38 +0200

    problem

---
+++ Leetcode/Medium/1283. Find the Smallest Divisor Given a Threshold/1283. Find the Smallest Divisor Given a Threshold/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _1283._Find_the_Smallest_Divisor_Given_a_Threshold
+{
+
+    public class Solution
+    {
+        // time  : O(n log m)
+        // space : O(1)
+        public int SmallestDivisor(int[] nums, int threshold)
+        {
+            int answer = 0;
+            Array.Sort(nums);
+            int end = nums[nums.Length -1 ];
+            int start = 0;
+            while (start <= end) 
+            {
+                int mid = (start + end) / 2;
+
+                double temp = 0;
+                for (global::System.Int32 i = nums.Length -1 ; i >=0; i--)
+                {
+                    double division = (double)nums[i] / mid;
+                    temp += Math.Ceiling(division);
+                    if(temp > threshold)
+                    {
+                        break;
+                    }
+                }
+                if(temp > threshold)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                    answer = mid;
+                }
+            }
+            return answer;
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.SmallestDivisor([5,9,2, 1], 6));
+        }
+    }
+}
---
+++ Leetcode/Medium/475. Heaters/475. Heaters/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace _475._Heaters
+{
+    public class Solution
+    {
+
+        // time  : O(n log m )
+        // space : O(1)
+        public int FindRadius(int[] houses, int[] heaters)
+        {
+            Array.Sort(heaters);
+            int maxRadius = 0;
+
+            foreach (int house in houses)
+            {
+                int closestDist = FindClosestHeaterDistance(house, heaters);
+                maxRadius = Math.Max(maxRadius, closestDist);
+            }
+
+            return maxRadius;
+        }
+
+        private int FindClosestHeaterDistance(int house, int[] heaters)
+        {
+            int left = 0, right = heaters.Length - 1;
+
+            
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (heaters[mid] == house)
+                {
+                    return 0;
+                }
+                else if (heaters[mid] < house)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+
+         
+            int distLeft = (right >= 0) ? Math.Abs(house - heaters[right]) : int.MaxValue;
+            int distRight = (left < heaters.Length) ? Math.Abs(house - heaters[left]) : int.MaxValue;
+
+            return Math.Min(distLeft, distRight);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.FindRadius([1, 1, 1, 1, 1, 1, 999, 999, 999, 999, 999],[499, 500, 501]));
+        }
+    }
+}
