e350914233959ba7294f364a1e888552d3f7a1af
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-04-18 14:44:11 +0200

    two problems

---
+++ Leetcode/Easy/100. Same Tree/100. Same Tree/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Xml.Linq;
+
+namespace _100._Same_Tree
+{
+ 
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+            
+        }
+    }
+    public class Solution
+    {
+        public bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == q) return true;
+            if (p == null) return false;
+            if (q == null) return false;
+            bool answer = true;
+            if (p.val == q.val)
+            {
+                if (p.left != null && q.left != null && answer)
+                {
+                    answer = answer && IsSameTree(p.left, q.left);
+
+                }
+                else if (p.left != q.left && answer)
+                {
+                    answer = false;
+
+                }
+                if (p.right != null && q.right != null && answer)
+                {
+                    answer = answer && IsSameTree(p.right, q.right);
+
+                }
+                else if (p.right  != q.right && answer)
+                {
+                    answer = false;
+                }
+            }
+            else
+            {
+                answer = false;
+            }
+
+            return answer;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            TreeNode node2 = new TreeNode(2);
+            TreeNode node3 = new TreeNode(3);
+
+            TreeNode root = new TreeNode(1, node2, null);
+            TreeNode root2 = new TreeNode(1, node2, null);
+            Console.WriteLine(s.IsSameTree(root, root2));
+
+        }
+    }
+}
---
+++ Leetcode/Easy/101. Symmetric Tree/101. Symmetric Tree/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _101._Symmetric_Tree
+{
+
+    public class Solution
+    {
+        public bool IsSymmetric(TreeNode root)
+        {
+            bool first = root.left == null;
+            bool second= root.right== null;
+
+            bool answer = true;
+            if (root == null) return false;
+            if (first && second) return true;
+            if (first || second) return false;
+            if(root.left.val != root.right.val) return false;
+            answer = check(root.left, root.right);
+            return answer;
+        }
+        public bool check(TreeNode left, TreeNode right)
+        {
+            bool answer = true;
+            if(left.val != right.val ) return false;
+            if(left.left != null && right.right != null)
+            {
+                answer = answer &&check(left.left, right.right);
+            }
+            else if((left.left != null && right.right == null)|| (left.left == null && right.right != null))
+            {
+                answer = false;
+            }
+            if (left.right != null && right.left!= null)
+            {
+                answer = answer && check(left.right, right.left);
+            }
+            else if ((left.right != null && right.left == null) || (left.right == null && right.left != null))
+            {
+                answer = false;
+            }
+            return answer;
+        }
+    }
+    public class TreeNode
+    {
+      public int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) 
+      {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+    }
+ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            TreeNode node3 = new TreeNode(3);
+            TreeNode node4 = new TreeNode(4);
+            TreeNode node2l = new TreeNode(2, node3, node4);
+            TreeNode node2r = new TreeNode(2, node4, node3);
+            TreeNode root = new TreeNode(1, node2l, node2r);
+            Console.WriteLine( solution.IsSymmetric(root));
+        }
+
+    }
+}
