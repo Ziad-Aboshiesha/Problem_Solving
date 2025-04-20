acb2145a1e8fa46945b3f18b201111d8be4cd0d3
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-04-20 14:23:01 +0200

    daily

---
+++ Leetcode/Medium/2563. Count the Number of Fair Pairs/2563. Count the Number of Fair Pairs/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace _2563._Count_the_Number_of_Fair_Pairs
+{
+    public class Solution
+    {
+        // time  : O(n log n)
+        // space : O(1)
+        public long CountFairPairs(int[] nums, int lower, int upper)
+        {
+            long count = 0;
+            Array.Sort(nums);
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                int first = i + 1;
+                int end = nums.Length - 1;
+                int mid=0, mid2=0;
+                int answer1 = 0;
+                int answer2 = 0;
+                while(first <=end)
+                {
+                    mid = (first + end) / 2;
+                    if (nums[i] + nums[mid] >= lower)
+                    {
+                        end = mid - 1;
+                        answer1 =mid;
+                    }
+                    else
+                    {
+                        first = mid + 1;
+                    }
+                }
+
+                first = i + 1;
+                end = nums.Length - 1;
+
+                while (first <= end)
+                {
+                    mid2 = (first + end) / 2;
+                    if (nums[i] + nums[mid2] <= upper)
+                    {
+                        first = mid2 + 1;
+                        answer2 =mid2;
+                    }
+                    else
+                    {
+                        end = mid2 - 1;
+                    }
+                }
+                if(answer1 != 0 && answer2 != 0 )
+                count += answer2 - answer1 + 1;
+            }
+
+            return count;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine( s.CountFairPairs([0, 1, 7, 4, 4, 5],3,6));
+        }
+    }
+}
---
+++ Leetcode/Medium/38. Count and Say/38. Count and Say/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace _38._Count_and_Say
+{
+    public class Solution
+    {
+        public string CountAndSay(int n)
+        {
+            string s = "";
+            string result = "";
+            for (int i = 0; i < n; i++)
+            {
+                if (s == "")
+                {
+                    s = "1";
+                    continue;
+                }
+                if (s == "1")
+                {
+                    s = "11";
+                    continue;
+                }
+                int occ = 1;
+                char temp = s[0];
+                result = "";
+                for (int j = 0; j < s.Length - 1; j++)
+                {
+                    if (s[j + 1] == temp)
+                    {
+                        occ++;
+                        continue;
+                    }
+                    else
+                    {
+                        for (int k = 0; k < occ; k++)
+                        {
+                            result = temp + result;
+                        }
+                        temp = s[j + 1];
+                        occ = 1;
+                    }
+                }
+                s = result;
+            }
+            return result;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.CountAndSay(4));
+        }
+    }
+}
