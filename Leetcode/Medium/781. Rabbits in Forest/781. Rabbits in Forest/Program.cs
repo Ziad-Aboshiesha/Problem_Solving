d35d4e41caa8e620443ce04682594b44b7fd665b
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-04-20 14:43:41 +0200

    daily rabbits

---
+++ Leetcode/Medium/781. Rabbits in Forest/781. Rabbits in Forest/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _781._Rabbits_in_Forest
+{
+
+    public class Solution
+    {
+        // time  : O(n)
+        // space : O(n)
+        public int NumRabbits(int[] answers)
+        {
+            Dictionary<int, int> count_occurance = new Dictionary<int, int>();
+            int count = 0 ;
+            for (int i = 0; i < answers.Length; i++)
+            {
+                if (count_occurance.ContainsKey(answers[i]))
+                {
+                    count_occurance[answers[i]]++;
+                }
+                else
+                {
+                    count_occurance[answers[i]] = 1;
+                }
+            }
+            foreach (int item in count_occurance.Keys)
+            {
+                if (count_occurance[item] == 1)
+                {
+                    count += item + 1;
+                } 
+                else
+                {
+                    int temp = (int)Math.Ceiling((double)count_occurance[item] / (item + 1));
+                    count += (item +  1) * temp;
+                }
+            }
+
+            return count ;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.NumRabbits([10,10,10]));
+        }
+    }
+}
