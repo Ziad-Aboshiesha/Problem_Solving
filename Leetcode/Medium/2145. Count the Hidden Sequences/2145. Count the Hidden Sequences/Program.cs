12815e82dcb7af3badf6ad9dc31081afc48540f1
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-04-21 05:52:01 +0200

    daily

---
+++ Leetcode/Medium/2145. Count the Hidden Sequences/2145. Count the Hidden Sequences/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _2145._Count_the_Hidden_Sequences
+{
+    public class Solution
+    {
+        public int NumberOfArrays(int[] differences, int lower, int upper)
+        {
+            long m = upper - lower + 1;
+            long answer = 0;
+            long[] arr = new long[differences.Length + 1];
+            long min = differences[0], max = differences[0];
+            arr[0] = differences[0];
+            for (int i = 0; i < differences.Length; i++)
+            {
+                arr[i + 1] = arr[i] + differences[i];
+                if (arr[i + 1] > max) max = arr[i + 1];
+                if (arr[i + 1] < min) min = arr[i + 1];
+
+            }
+            if (m > (max - min))
+                answer = m - (max - min);
+
+            return (int)answer;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+        }
+    }
+}
