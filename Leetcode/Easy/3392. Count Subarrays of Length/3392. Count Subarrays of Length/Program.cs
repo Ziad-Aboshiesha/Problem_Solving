12a0215ae43a3d857ac0f9fe32b9c9c13d737798
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-04-27 08:05:21 +0300

    daily

---
+++ Leetcode/Easy/3392. Count Subarrays of Length/3392. Count Subarrays of Length/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _3392._Count_Subarrays_of_Length
+{
+    public class Solution
+    {
+        // time  : O(n)
+        // space : O(1)
+        public int CountSubarrays(int[] nums)
+        {
+            int count = 0;
+            
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int j = i + 2;
+                if (nums[i] + nums[j] == nums[i+1]/2.0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.CountSubarrays([1, 2, 1, 4, 1]));
+        }
+    }
+}
