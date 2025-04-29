5f319b006369d7e777623ca0e2efe2a41ed9cba7
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-04-29 22:18:35 +0300

    daily

---
+++ Leetcode/Easy/3110. Score of a String/3110. Score of a String/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _3110._Score_of_a_String
+{
+    public class Solution
+    {
+        public int ScoreOfString(string s)
+        {
+            int sum = 0;
+
+            if(s.Length<2) return sum;
+            for (int i = 0; i < s.Length -1 ; i++)
+            {
+                sum += Math.Abs((int)s[i] - (int)s[ i + 1 ]);
+            }
+
+            return sum;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Solution s = new Solution();
+            Console.WriteLine(s.ScoreOfString("zaz"));
+        }
+    }
+}
---
+++ Leetcode/Medium/2962. Count Subarrays Where Max/2962. Count Subarrays Where Max/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _2962._Count_Subarrays_Where_Max
+{
+    public class Solution
+    {
+        // time  : O(n)
+        // space : O(1)
+        public long CountSubarrays(int[] nums, int k)
+        {
+            int max = nums.Max();
+            if (nums.Length == 1 && k == 1) return 1;
+            if (k == 0 || nums.Length == 0) return 0;
+            long count = 0;
+            int occurance = 0;
+            int right = 0;
+            int left = 0;
+            for (right = 0; right < nums.Length; right++)
+            {
+                if (nums[right] == max)
+                {
+                    occurance++;
+                }
+                while (occurance >= k)
+                {
+                    count += nums.Length - right;
+                    if (nums[left] == max)
+                        occurance--;
+                    left++;
+                }
+            }
+            return count;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine( s.CountSubarrays([1, 4, 2, 1],  3));
+        }
+    }
+}
