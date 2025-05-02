0e7abef2cdb5167bd57b330eb2dfd73b1f815610
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-02 15:10:40 +0300

    easy

---
+++ Leetcode/Easy/2769. Find the Maximum Achievable Number/2769. Find the Maximum Achievable Number/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _2769._Find_the_Maximum_Achievable_Number
+{
+    public class Solution
+    {
+        // time  : O(1)
+        // space : O(1)
+        public int TheMaximumAchievableX(int num, int t)
+        {
+            return t * 2 + num;
+        }
+    }
+
+    internal class Program
+    {
+        
+    }
+}
