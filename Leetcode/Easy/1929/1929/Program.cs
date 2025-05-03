451bb2113e69fd9e7a7e20405bc4249fbc729ceb
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-03 10:35:22 +0300

    easy

---
+++ Leetcode/Easy/1929/1929/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _1929
+{
+    public class Solution
+    {
+        public int[] GetConcatenation(int[] nums)
+        {
+            int[] arr = new int[2 * nums.Length];
+            int n = arr.Length / 2;
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = nums[i];
+                arr[i + n] = nums[i];
+            }
+
+            return arr;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
