d7c5011b7cf93369fefa36157f4a16977c482899
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-05 05:58:14 +0300

    problem easy

---
+++ Leetcode/Easy/1920. Build Array from Permutation/1920. Build Array from Permutation/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _1920._Build_Array_from_Permutation
+{
+    public class Solution
+    {
+        // time  : O(n)
+        // space : O(n)
+        public int[] BuildArray(int[] nums)
+        {
+            int[] arr = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                arr[i] = nums[nums[i]];
+            }
+            return arr;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
