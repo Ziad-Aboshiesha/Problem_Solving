b8383f7a5bf04b5ec6188f3b0baae6bf6fa3af2f
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-07 06:39:02 +0300

    sort colors

---
+++ Leetcode/Medium/75. Sort Colors/75. Sort Colors/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _75._Sort_Colors
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+            Array.Sort(nums);
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
