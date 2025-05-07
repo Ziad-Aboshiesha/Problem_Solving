5ad9a9792de7e525638b2ae75189db6763eb8028
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-07 07:06:32 +0300

    Container With Most Water

---
+++ Leetcode/Medium/11. Container With Most Water/11. Container With Most Water/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _11._Container_With_Most_Water
+{
+    public class Solution
+    {
+        // time  : O(n)
+        // space : O(1)
+        public int MaxArea(int[] height)
+        {
+            int left = 0;
+            int right = height.Length - 1;
+            int area = 0;
+
+            while (left < right) 
+            {
+                int temp = (right - left) * Math.Min(height[left], height[right]);
+                if(temp > area) {area = temp;}
+                if (height[left] > height[right])
+                {
+                    right--;
+                }
+                else 
+                {
+                    left++;
+                }
+            }
+
+
+            return area;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
