f952894af68c1d7ba6514731f5a31872eb092636
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-08 19:28:05 +0300

    771. Jewels and Stones

---
+++ Leetcode/Easy/771. Jewels and Stones/771. Jewels and Stones/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _771._Jewels_and_Stones
+{
+
+    public class Solution
+    {
+        // time  : O(n*m)
+        // space : O(1)
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+
+            int count = 0;
+            foreach (char stone in stones)
+            {
+                if (jewels.Contains(stone))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
