39cc5cf62cfc78f8b6743a48fb47bb40da221bf0
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-09 11:52:17 +0300

    reverse string

---
+++ Leetcode/Easy/344. Reverse String/344. Reverse String/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _344._Reverse_String
+{
+    public class Solution
+    {
+        // time  : O(n)
+        // space : O(1)
+        public void ReverseString(char[] s)
+        {
+
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                char temp = s[i];
+                s[i] = s[s.Length - i - 1];
+                s[s.Length - i - 1] = temp;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
