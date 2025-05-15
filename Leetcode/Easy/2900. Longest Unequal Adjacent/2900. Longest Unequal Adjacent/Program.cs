16c5741b8c81c469b067a6de38ba3a47ad2af3d4
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-15 06:00:12 +0300

    daily

---
+++ Leetcode/Easy/2900. Longest Unequal Adjacent/2900. Longest Unequal Adjacent/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _2900._Longest_Unequal_Adjacent
+{
+    public class Solution
+    {
+        // time  : O(n)
+        // space : O(n)
+        public IList<string> GetLongestSubsequence(string[] words, int[] groups)
+        {
+            IList<string> result = new List<string>();
+            result.Add(words[0]);
+            for (int i = 0; i < groups.Length - 1; i++)
+            {
+                if (groups[i] != groups[i+1])
+                {
+                    result.Add(words[i + 1]);
+                }
+            }
+
+            return result;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            IList<string> ans = solution.GetLongestSubsequence(["a", "b", "c", "d", "e", "f", "g", "h"], [1, 0, 1, 1, 0, 1, 0, 1]);
+            for (int i = 0; i < ans.Count; i++)
+            {
+                Console.Write(ans[i] + " ");
+            }
+        }
+    }
+}
