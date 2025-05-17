dc5abd3bff9f17d6601d4b13b6cead9e3c786ff4
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-17 14:19:05 +0300

    easy one

---
+++ Leetcode/Easy/258. Add Digits/258. Add Digits/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _258._Add_Digits
+{
+
+    public class Solution
+    {
+        // time  : O(log n) 
+        // space : O(1)
+        public int AddDigits(int num)
+        {
+            int sum = 11;
+            while (sum >= 10)
+            {
+                sum = 0;
+                while (num != 0)
+                {
+                    int temp = num % 10;
+                    sum += temp;
+                    num = num / 10;
+                }
+                num = sum;
+            }
+
+
+            return sum;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
