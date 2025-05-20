2f3296da8593d749e7a4dc6d28ec753183b5aa19
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-20 07:21:26 +0300

    daily

---
+++ Leetcode/Medium/3355. Zero Array Transformation I/3355. Zero Array Transformation I/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _3355._Zero_Array_Transformation_I
+{
+    public class Solution
+    {
+        // time  : O(n+m)
+        // space : O(n)
+        /*
+         * you have query with start and end 
+         * when you reach start add one and when you reach end minus one from the next element 
+         * so now you can know every element will be decreament how many times 
+         * [4,3,2,1]
+         * [0,3]
+         * in this case the help will be [1,1,1,1,-1]
+         * so to apply it make variable save value of decreament
+         * if we add one query [1,2]
+         * help will be [1,2,2,1,-1]
+         */
+        public bool IsZeroArray(int[] nums, int[][] queries)
+        {
+
+            int[] help = new int[nums.Length + 1]; // define array which length is bigger than nums
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int start = queries[i][0];
+                int end = queries[i][1];
+
+                help[start] += 1;
+                help[end + 1] -= 1;
+
+            }
+            int minus = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                minus += help[i];
+                if (nums[i] > minus)
+                {
+                    return false;
+                }
+            }
+            return true;
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Leetcode/Medium/64. Minimum Path Sum/64. Minimum Path Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _64._Minimum_Path_Sum
+{
+    public class Solution
+    {
+        public int MinPathSum(int[][] grid)
+        {
+            
+            int m = grid.Length;
+            int n = grid[0].Length;
+            int[,] dp = new int[m, n];
+            dp[m - 1, n - 1] = grid[m - 1][n - 1];
+            for (int i = m -1; i >=0;i--)
+            {
+                for (int j = n -1; j >=0; j--)
+                {
+                    if(i==m-1 && j==n-1)
+                    {
+                        continue;
+                    }
+                    if(i==m-1)
+                    {
+                        dp[i, j] =grid[i][j] + dp[i, j + 1];
+                        continue;
+                    }
+                    if (j == n - 1)
+                    {
+                        dp[i, j] = grid[i][j] + dp[i+1, j ];
+                        continue;
+                    }
+                    dp[i, j] = grid[i][j] + Math.Min(dp[i, j + 1], dp[i + 1, j]);
+                }
+            }
+            return dp[0, 0];
+
+
+
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.MinPathSum([[1, 2, 3], [4, 5, 6]]));
+        }
+    }
+}
