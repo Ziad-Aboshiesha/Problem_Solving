c6671ffc87c7c115bca286763744185b76598459
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-26 18:07:43 +0300

    medium

---
+++ Leetcode/Medium/3365. Rearrange K Substrings t/3365. Rearrange K Substrings/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+Solution s = new Solution();
+Console.WriteLine(s.IsPossibleToRearrange("aabbcc", "bbaacc",  2));
+
+public class Solution
+{
+    // time : O(n)
+    // space : O(n)
+    public bool IsPossibleToRearrange(string s, string t, int k)
+    {
+        int divider = s.Length / k;
+        if (s.Length != t.Length) return false;
+        if (s == t) return true;
+        Dictionary<string, int> strings = new Dictionary<string, int>();
+        for (int i = 0; i < t.Length; i += divider)
+        {
+            string temp = t.Substring(i, divider);
+            if (strings.ContainsKey(temp))
+            {
+                strings[temp] += 1;
+
+            }
+            else
+            {
+                strings[temp] = 1;
+
+            }
+        }
+        for (int i = 0; i < s.Length; i += divider)
+        {
+            string temp = s.Substring(i, divider);
+            if (strings.ContainsKey(temp))
+            {
+                if (strings[temp] > 1)
+                {
+
+                    strings[temp] -= 1;
+                }
+                else
+                {
+                    strings.Remove(temp);
+                }
+                continue;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
