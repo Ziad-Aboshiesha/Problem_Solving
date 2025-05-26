889468e2753265c3a585baa47614be36f782277f
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-26 18:20:10 +0300

    easy chess

---
+++ Leetcode/Easy/1812. Determine Color of/1812. Determine Color of a Chessboar/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Solution {
+    public bool SquareIsWhite(string coordinates) {
+        char temp = coordinates[0];
+        int num = coordinates[1];
+        if(((int) temp %2 != 0 && num%2 != 0)||((int) temp %2 == 0 && num%2 == 0) )
+        {
+                return false;
+        }
+        return true;
+    }
+}
