3dd9629221006cfe814fcbde1fedd6e7f19d6ef1
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-27 12:42:09 +0300

    daily

---
+++ Leetcode/Easy/2894. Divisible and Non-divisible/2894. Divisible and Non-divisible/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public int DifferenceOfSums(int n, int m)
+    {
+        int num1=0, num2 = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % m == 0)
+            {
+                num2 += i;
+            }
+        }
+
+        return num1 - num2;
+    }
+}
