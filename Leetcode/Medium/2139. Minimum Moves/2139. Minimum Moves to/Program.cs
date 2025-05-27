cd4e3b8bca4e4e009c456bcbc948a5eccf452f7d
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-27 12:52:19 +0300

    daily

---
+++ Leetcode/Medium/2139. Minimum Moves/2139. Minimum Moves to/Program.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public int MinMoves(int target, int maxDoubles)
+    {
+        int count_opertions = 0;
+        if (target == 1) return count_opertions;
+        while (maxDoubles > 0 && target != 1)
+        {
+            if (target % 2 == 0)
+            {
+                target /= 2;
+                count_opertions++;
+                maxDoubles--;
+            }
+            else
+            {
+                target--;
+                count_opertions++;
+            }
+        }
+        return count_opertions + (target - 1);
+    }
+}
