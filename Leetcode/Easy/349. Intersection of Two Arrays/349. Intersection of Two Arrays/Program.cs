935c96584c6343874481863ec41bf2ca217397f2
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-28 11:35:42 +0300

    easy

---
+++ Leetcode/Easy/349. Intersection of Two Arrays/349. Intersection of Two Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        HashSet<int> one = nums1.ToHashSet();
+        HashSet<int> two = nums2.ToHashSet();
+        if (one.Count() > two.Count())
+        {
+            return compare(one, two).ToArray();
+        }
+        else
+        {
+            return compare( two, one).ToArray();
+            
+        }
+    }
+    public HashSet<int> compare(HashSet<int> big, HashSet<int> small)
+    {
+        foreach (var item in small)
+        {
+            if (!big.Contains(item))
+            {
+                small.Remove(item);
+            }
+        }
+        return small;
+    }
+}
