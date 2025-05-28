b7f9cb09b96929877e7caae31f0b6ce082807e6e
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-28 11:43:48 +0300

    easy

---
+++ Leetcode/Easy/191. Number of 1 Bits/191. Number of 1 Bits/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+Solution s = new Solution();
+Console.WriteLine(s.HammingWeight(11));
+public class Solution
+{
+    public int HammingWeight(int n)
+    {
+        int result = 0;
+        string binaryString = Convert.ToString(n, 2);
+        for (int i = 0; i < binaryString.Length; i++)
+        {
+            if (binaryString[i] == '1')
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+}
