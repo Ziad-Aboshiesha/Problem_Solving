e569f3e1a83302808e0ebc77c2f3625a25b10750
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-05-30 20:23:49 +0300

    easy

---
+++ Leetcode/Easy/1374. Generate a String With Characters That Have Odd Counts/1374. Generate a/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Solution {
+    public string GenerateTheString(int n) {
+    if (n % 2 == 0)
+        return new string('a', n - 1) + 'b';
+    else
+        return new string('a', n);
+}
+}
