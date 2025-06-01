f329c25c61aed7ddcd15bf24078552ad3afbb98d
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-02 02:17:30 +0300

    med

---
+++ Leetcode/Medium/347. Top K Frequent Elements/347. Top K Frequent Elements/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _347._Top_K_Frequent_Elements
+{
+    public class Solution
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            Dictionary<int, int > res = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res.TryAdd(nums[i], 0);
+                res[nums[i]]++;
+            }
+            return res
+           .OrderByDescending(pair => pair.Value)
+           .Take(k)
+           .Select(pair => pair.Key)
+           .ToArray();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
