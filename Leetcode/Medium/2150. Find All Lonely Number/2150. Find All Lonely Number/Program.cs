d62ffb7acc0fad5e884aa840c59b47e7823e9230
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-01 21:56:20 +0300

    med

---
+++ Leetcode/Medium/2150. Find All Lonely Number/2150. Find All Lonely Number/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _2150._Find_All_Lonely_Number
+{
+    public class Solution
+    {
+        public IList<int> FindLonely(int[] nums)
+        {
+            Dictionary<int, int> res = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res.TryAdd(nums[i], 0);
+                res[nums[i]] += 1;
+            }
+            List<int> result = new List<int>();
+            foreach (var item in res)
+            {
+                if( item.Value >1 || res.ContainsKey(item.Key-1) || res.ContainsKey(item.Key + 1)) continue;
+                result.Add(item.Key);
+            }
+            return result;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Leetcode/Medium/535. Encode and Decode TinyURL/535. Encode and Decode TinyURL/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+namespace _535._Encode_and_Decode_TinyURL
+{
+    public class Codec
+    {
+        
+        Hashtable hashtable  = new Hashtable();
+        // Encodes a URL to a shortened URL
+        public string encode(string longUrl)
+        {
+
+        }
+
+        // Decodes a shortened URL to its original URL.
+        public string decode(string shortUrl)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
