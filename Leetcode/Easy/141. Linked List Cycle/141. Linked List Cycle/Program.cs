2023b68eee6e266abb168f9d5dee1a1e168aa7e3
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-02 01:27:35 +0300

    easy

---
+++ Leetcode/Easy/141. Linked List Cycle/141. Linked List Cycle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+
+namespace _141._Linked_List_Cycle
+{
+   
+  
+   public class ListNode 
+   {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) {
+          val = x;
+          next = null;
+      }
+   }
+ 
+    public class Solution
+    {
+        public bool HasCycle(ListNode head)
+        {
+            HashSet<ListNode> visited = new HashSet<ListNode>();
+            while (head != null) 
+            {
+                if(visited.Contains(head))
+                {
+                    return true;
+                }
+                else
+                {
+                    visited.Add(head);
+                }
+                head = head.next;
+            }
+            return false;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
