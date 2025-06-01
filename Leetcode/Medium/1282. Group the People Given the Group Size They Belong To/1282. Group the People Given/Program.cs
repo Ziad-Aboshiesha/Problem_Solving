51204a9b27d8b3400cd509dc6e3e1f35f17012de
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-01 11:41:43 +0300

    med

---
+++ Leetcode/Medium/1282. Group the People Given the Group Size They Belong To/1282. Group the People Given/Program.cs
@@ -0,0 +1,36 @@
+﻿Solution s = new Solution();
+s.GroupThePeople([3, 3, 3, 3, 3, 1, 3]);
+public class Solution
+{
+    public IList<IList<int>> GroupThePeople(int[] groupSizes)
+    {
+        Dictionary<int, List<int>> res = new Dictionary<int, List<int>>();
+        for (int i = 0; i < groupSizes.Length; i++)
+        {
+            if (!res.ContainsKey(groupSizes[i]))
+            {
+                res[groupSizes[i]] = new List<int>();
+            }
+
+            res[groupSizes[i]].Add(i);
+        }
+        List<List<int>> result = new List<List<int>>();
+        foreach (KeyValuePair<int, List<int>> item in res)
+        {
+            if (item.Key == item.Value.Count)
+            {
+                result.Add(item.Value);
+            }
+            else
+            {
+                int partSize = item.Value.Count / item.Key;
+
+                for (int i = 0; i < partSize; i++)
+                {
+                    result.Add(item.Value.GetRange(i * item.Key, item.Key));
+                }
+            }
+        }
+        return result.Cast<IList<int>>().ToList();
+    }
+}
