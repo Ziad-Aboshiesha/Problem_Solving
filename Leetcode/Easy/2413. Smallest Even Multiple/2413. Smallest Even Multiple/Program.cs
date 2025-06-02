642edbc386fe3a116cb992e01ce52cd40fbf876e
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-02 06:49:53 +0300

    easy

---
+++ Leetcode/Easy/2413. Smallest Even Multiple/2413. Smallest Even Multiple/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _2413._Smallest_Even_Multiple
+{
+    public class Solution
+    {
+        public int SmallestEvenMultiple(int n)
+        {
+            if (n % 2 == 0) return n;
+            return n * 2;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Leetcode/Medium/2007. Find Original Array From/2007. Find Original Array From/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _2007._Find_Original_Array_From
+{
+    public class Solution
+    {
+        public int[] FindOriginalArray(int[] changed)
+        {
+            List<int> r = new List<int>();
+            List<int> result = changed.ToList();
+            result.Sort();
+            HashSet<int> visited = result.ToHashSet();
+            for (int i = 0; i < result.Count; i++)
+            {
+                if (visited.Contains(result[i] ) && visited.Contains(result[i] *2))
+                {
+                    r.Add(result[i]);
+                    visited.Remove(result[i]);
+                    visited.Remove(result[i] * 2);
+
+                }
+            }
+            if (visited.Count > 0) { return []; }
+            return r.ToArray();
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            s.FindOriginalArray([1, 3, 4, 2, 6, 8]);
+        }
+    }
+}
