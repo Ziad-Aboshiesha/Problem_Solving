de9f5f596b6c5d02bb71788ee135a5b247ac9915
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-03 09:24:28 +0300

    1936

---
+++ Leetcode/Medium/1936. Add Minimum Number of Rungs/1936. Add Minimum Number of Rungs/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _1936._Add_Minimum_Number_of_Rungs
+{
+    public class Solution
+    {
+        public int AddRungs(int[] rungs, int dist)
+        {
+            int current = 0;
+            int count = 0;
+            for (int i = 0; i < rungs.Length; i++)
+            {
+                if (rungs[i] - current <= dist)
+                {
+                    current = rungs[i];
+                    continue;
+                }
+                else
+                {
+                    count += (rungs[i] - current - 1) / dist;
+                    current = rungs[i];
+                }
+            }
+            return count;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
