6c2ea5de5f27ed5524a328e7bf0f56366ae926b4
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-04 19:22:27 +0300

    med

---
+++ Leetcode/Medium/1814. Count Nice Pairs in an Array/1814. Count Nice Pairs in an Array/Program.cs
@@ -0,0 +1,34 @@
+﻿public class Solution
+{
+    public int CountNicePairs(int[] nums)
+    {
+        int count = 0;
+        Dictionary<int, int> pairs = new();
+        // int[] rev = new int[nums.Length];
+        for (int i = 0; i < nums.Length; i++)
+        {
+            // rev[i] = reverse(nums[i]);
+            int temp = nums[i] - reverse(nums[i]);
+            pairs.TryAdd(temp, 0);
+            pairs[temp] += 1;
+        }
+
+        foreach (var item in pairs)
+        {
+            count += item.Value * (item.Value - 1) / 2;
+        }
+
+        return count;
+    }
+    private int reverse(int x)
+    {
+        int res = 0;
+        while (x != 0)
+        {
+            int temp = x % 10;
+            res = res * 10 + temp;
+            x = x / 10;
+        }
+        return res;
+    }
+}
---
+++ Leetcode/Medium/2249. Count Lattice Points Inside a Circle/2249. Count Lattice Points Inside a Circle/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int CountLatticePoints(int[][] circles)
+    {
+        HashSet<string> res = new HashSet<string>();
+        for (int i = 0; i < circles.Length; i++)
+        {
+            int x = circles[i][0];
+            int y = circles[i][1];
+            int r = circles[i][2];
+
+            for (int j = x - r; j <= x + r; j++)
+            {
+                for (int k = y - r; k <= y + r; k++)
+                {
+                    if ((x - j) * (x - j) + (y - k) * (y - k) <= r * r)
+                    {
+                        res.Add($"{j},{k}");
+                    }
+                }
+            }
+        }
+        return res.Count;
+    }
+}
