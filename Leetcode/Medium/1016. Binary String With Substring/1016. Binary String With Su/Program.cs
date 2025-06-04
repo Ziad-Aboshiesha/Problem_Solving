048e61af4d8d6cc819bd679f54b845a85e22b961
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-04 19:34:54 +0300

    1016

---
+++ Leetcode/Medium/1016. Binary String With Substring/1016. Binary String With Su/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public bool QueryString(string s, int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            string binary = Convert.ToString(i, 2);
+            if (!s.Contains(binary))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
