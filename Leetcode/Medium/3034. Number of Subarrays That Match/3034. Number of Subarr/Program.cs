22da1d90732191bb3b8891b31ba6063e669d6773
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-04 19:48:38 +0300

    3034

---
+++ Leetcode/Medium/3034. Number of Subarrays That Match/3034. Number of Subarr/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    public int CountMatchingSubarrays(int[] nums, int[] pattern)
+    {
+        int count = 0;
+        for (int i = 0; i < nums.Length - pattern.Length; i++)
+        {
+            bool status = true;
+
+            for (int j = 0; j < pattern.Length; j++)
+            {
+                if (!(pattern[j] == 0 && nums[i + j + 1] == nums[i + j]
+                || pattern[j] == -1 && nums[i + j + 1] < nums[i + j]
+                || pattern[j] == 1 && nums[i + j + 1] > nums[i + j]))
+                {
+                    status = false;
+                    break;
+                }
+            }
+            if (status)
+            {
+                count += 1;
+            }
+        }
+        return count;
+    }
+}
