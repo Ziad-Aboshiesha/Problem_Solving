f7045470214af17e5a601271d6964539e6fa76cd
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-06 05:33:23 +0300

    1209

---
+++ Leetcode/Medium/1209. Remove All Adjacent Duplicates/1209. Remove All Adjacent Dup/Program.cs
@@ -0,0 +1,50 @@
+﻿public class Solution
+{
+    public string RemoveDuplicates(string s, int k)
+    {
+        string res = "";
+        Stack<special> ans = new Stack<special>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (ans.Count == 0)
+            {
+                ans.Push(new special(s[i]));
+            }
+            else
+            {
+                special temp = ans.Peek();
+                if (s[i] == temp.c)
+                {
+                    temp.count++;
+                    if (temp.count == k)
+                    {
+                        ans.Pop();
+                    }
+                }
+                else
+                {
+                    ans.Push(new special(s[i]));
+                }
+            }
+        }
+        while (ans.Count != 0)
+        {
+            special temp = ans.Pop();
+            for (int i = 0; i < temp.count; i++)
+            {
+                res += temp.c;
+            }
+        }
+        return res;
+    }
+}
+public class special
+{
+    public char c= '\0';
+    public int count = 0;
+    public special(char c)
+    {
+        this.c = c;
+        count = 1;
+    }
+}
