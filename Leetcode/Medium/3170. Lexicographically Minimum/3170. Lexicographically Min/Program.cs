eb42721f3dbffdd57255add842e378d60d37716f
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-08 20:19:33 +0300

    386

---
+++ Leetcode/Medium/3170. Lexicographically Minimum/3170. Lexicographically Min/Program.cs
@@ -0,0 +1,37 @@
+﻿Solution s = new Solution();
+System.Console.WriteLine(   s.ClearStars("abc*de*fgh*"));
+string ss = "safasdf";
+public class Solution
+{
+    public string ClearStars(string s)
+    {
+        List<char> arr = s.ToList();
+        HashSet<int> dl = new();
+        PriorityQueue<int, (int p, int idx)> pq = new();
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (arr[i] != '*')
+            {
+                pq.Enqueue(i, ((int)arr[i], -i));
+            }
+            else
+            {
+                dl.Add(i);
+                dl.Add(pq.Dequeue());
+
+
+            }
+        }
+        for (int i = 0; i < arr.Count; i++)
+        {
+            if (dl.Contains(i))
+            {
+
+                arr[i] = '\0';
+            }
+        }
+        string  sss = new string(arr.ToArray());
+        return sss.Replace("\0", "");
+
+    }
+}
---
+++ Leetcode/Medium/386. Lexicographical Numbers/386. Lexicographical Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿Solution s = new Solution();
+IList<int> r = s.LexicalOrder(130);
+for (int i = 0; i < r.Count; i++)
+{
+    System.Console.WriteLine(r[i]);
+}
+public class Solution
+{
+    public IList<int> LexicalOrder(int n)
+    {
+        List<int> result = new(n);
+        int curr = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            result.Add(curr);
+
+            if (curr * 10 <= n)
+            {
+                curr *= 10; // Go deeper
+            }
+            else
+            {
+                // Next sibling or backtrack
+                if (curr >= n)
+                    curr /= 10;
+
+                curr += 1;
+
+                while (curr % 10 == 0)
+                    curr /= 10;
+            }
+        }
+
+        return result;
+    }
+}
