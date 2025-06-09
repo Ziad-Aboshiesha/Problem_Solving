e50136380520ea0351ab2ec28ba43ce11c724e08
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-09 17:16:38 +0300

    961

---
+++ Leetcode/Easy/961. N-Repeated Element in Size 2N Array/961. N-Repeated Element in Size 2N Array/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution {
+    public int RepeatedNTimes(int[] nums)
+    {
+        HashSet<int> numbers = new HashSet<int>();
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (numbers.Contains(nums[i]))
+            {
+                return nums[i];
+            }
+            numbers.Add(nums[i]);
+
+        }
+        return -1;    
+    }
+}
