410f3d9c5a7504580682715608c0fbf88550231f
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-10 09:02:52 +0300

    676

---
+++ Leetcode/Medium/676. Implement Magic Dictionary/676. Implement Magic Dictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿public class MagicDictionary {
+    Dictionary<string, string> dict; 
+    public MagicDictionary() {
+        dict = new Dictionary<string, string>();
+    }
+    
+    public void BuildDict(string[] dictionary) {
+        for (int i = 0; i < dictionary.Length  ; i++)
+        {
+            dict.Add(dictionary[i], dictionary[dictionary.Length - 1 ]);
+        }
+    }
+
+    public bool Search(string searchWord)
+    {
+        // if (dict.ContainsKey(searchWord))
+        // {
+        //     return false;
+        // }
+        foreach (var item in dict)
+        {
+            int diff = 0;
+            if (item.Key.Length != searchWord.Length)
+            {
+                continue;
+            }
+            else
+            {
+                for (int i = 0; i < searchWord.Length; i++)
+                {
+                    if (searchWord[i] != item.Key[i])
+                    {
+                        diff++;
+                        if (diff > 1) break;
+                    }
+                }
+            }
+            if (diff == 1) return true;
+        }
+        return false;
+        
+    }
+}
