4be416417bdad3c1fd3cd7c479ff8f6fb5abaeb6
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-10 08:40:56 +0300

    3442

---
+++ Leetcode/Easy/3442. Maximum Difference Between/3442. Maximum Differenc/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public int MaxDifference(string s)
+    {
+        int[] freq = new int[26];
+        foreach (var item in s)
+        {
+            freq[item -'a']++;
+        }
+        int  max_odd = 0, min_even = int.MaxValue;
+        for (int i = 0; i < 26; i++)
+        {
+            
+            if (freq[i] != 0 && freq[i] % 2 == 0 && freq[i] < min_even)
+            {
+                min_even = freq[i];
+            }
+        
+            if (freq[i] % 2 != 0 && freq[i] > max_odd)
+            {
+                max_odd = freq[i];
+            }
+        }
+        return  max_odd - min_even;
+    }
+}
---
+++ Leetcode/Medium/686. Repeated String Match/686. Repeated String Match/Program.cs
@@ -0,0 +1,45 @@
+﻿Solution s = new Solution();
+System.Console.WriteLine(s.RepeatedStringMatch("aaac","aac"));
+public class Solution
+{
+    public int RepeatedStringMatch(string a, string b)
+    {
+        if (b.Length == 0) return 0;
+        if(b.Length < a.Length && a.Contains(b)) return 1;
+        char first = b[0];
+        int current = -1;
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] == first)
+            {
+                current = i;
+                break;
+            }
+        }
+        int count = 0;
+        if (current == -1) return -1;
+        for (int i = 0; i < b.Length; i++)
+        {
+            // if (current == 0)
+            // {
+            //     count++;
+            // }
+            if (b[i] != a[current])
+                {
+                    return -1;
+                }
+            if (current == a.Length - 1)
+            {
+                current = 0;
+                count++;
+            }
+            else
+            {
+                current++;
+            }
+
+        }
+        if (current != 0) count++;
+        return count;
+    }
+}
