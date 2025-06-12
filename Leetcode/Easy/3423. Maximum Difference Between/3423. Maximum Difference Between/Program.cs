d9b90cbbf54c565b2a22f0a2271008ea26bf1643
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-12 05:04:57 +0300

    3423

---
+++ Leetcode/Easy/3423. Maximum Difference Between/3423. Maximum Difference Between/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public int MaxAdjacentDistance(int[] nums) {
+        int diff = 0;
+        for(int i = 0 ; i < nums.Length - 1; i++)
+        {
+            diff = Math.Max(diff, Math.Abs(nums[i+1] - nums[i]));
+        }
+        
+        diff = Math.Max(diff, Math.Abs(nums[nums.Length - 1 ] - nums[0]));
+        
+        return diff;
+    }
+}
