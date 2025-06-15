f1db93442c2aa30bb81f954b395ac5e3af50393b
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-15 17:04:27 +0300

    268

---
+++ Leetcode/Easy/268. Missing Number/268. Missing Number/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Immutable;
+
+public class Solution {
+    public int MissingNumber(int[] nums)
+    {
+        HashSet<int> ints = nums.ToHashSet<int>();
+        for (int i = 0; i <= nums.Length; i++)
+        {
+            if (!ints.Contains(i))
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
