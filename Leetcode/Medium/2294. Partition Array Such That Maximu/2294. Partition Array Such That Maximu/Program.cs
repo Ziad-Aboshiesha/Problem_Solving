ebc5ba58002a4708d4392fcbaaf45346bc8ba90d
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-19 15:21:42 +0300

    2294 daily med

---
+++ Leetcode/Medium/2294. Partition Array Such That Maximu/2294. Partition Array Such That Maximu/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public int PartitionArray(int[] nums, int k)
+    {
+        if (nums.Length == 0) return 0;
+        if (nums.Length == 1) return 1;
+        Array.Sort(nums);
+        int ans = 1;
+        int current = nums[0];
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] - current > k)
+            {
+                ans++;
+                current = nums[i];
+            }
+        }
+        return ans;
+    }
+}
