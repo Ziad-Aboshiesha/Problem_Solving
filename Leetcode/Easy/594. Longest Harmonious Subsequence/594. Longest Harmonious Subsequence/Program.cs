af0bdfea4dc51626416566751319edadea018933
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-06-30 14:31:13 +0300

    594

---
+++ Leetcode/Easy/594. Longest Harmonious Subsequence/594. Longest Harmonious Subsequence/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _594._Longest_Harmonious_Subsequence
+{
+    public class Solution
+    {
+        public int FindLHS(int[] nums)
+        {
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                keyValuePairs.TryAdd(nums[i], 0);
+                keyValuePairs[nums[i]]++;
+            }
+            int result = 0;
+            foreach (var item in keyValuePairs)
+            {
+                if(keyValuePairs.ContainsKey(item.Key-1))
+                {
+
+                    result = Math.Max(result, keyValuePairs[item.Key] + keyValuePairs[item.Key - 1 ]);
+                }
+                if (keyValuePairs.ContainsKey(item.Key + 1))
+                {
+
+                    result = Math.Max(result, keyValuePairs[item.Key] + keyValuePairs[item.Key + 1]);
+                }
+            }
+            return result;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
