26cae4d7644c5b07927713b9c22c4eea43c493e0
Ziad Aboshiesha <ziadaboshiesha3@gmail.com>
2025-07-15 09:02:59 +0300

    3136

---
+++ Leetcode/Medium/2410. Maximum Matching of Players With Trainers/2410. Maximum Matching of Players With Trainers/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public int MatchPlayersAndTrainers(int[] players, int[] trainers)
+    {
+        Array.Sort(players);
+        Array.Sort(trainers);
+        int ans = 0;
+        int idx = 0;
+        for (int i = 0; i < trainers.Length; i++)
+        {
+            if (players[idx] <= trainers[i])
+            {
+                ans++;
+                idx++;
+
+            }
+            if (idx == players.Length)
+            {
+                break;
+            }
+        }
+        return ans;
+        
+    }
+}
