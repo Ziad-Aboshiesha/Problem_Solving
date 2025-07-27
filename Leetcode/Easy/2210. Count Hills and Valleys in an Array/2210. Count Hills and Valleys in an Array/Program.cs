63886657558d5ab924c58d95ff10a7f61aa1c456
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-07-27 20:26:40 +0300

    2210

---
+++ Leetcode/Easy/2210. Count Hills and Valleys in an Array/2210. Count Hills and Valleys in an Array/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace _2210._Count_Hills_and_Valleys_in_an_Array
+{
+    public class Solution
+    {
+        public int CountHillValley(int[] nums)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(nums[0]);
+            for (int i = 1; i < nums.Length ; i++)
+            {
+                if(nums[i] != nums[i-1])
+                {
+                    numbers.Add(nums[i]);
+                }
+            }
+            int ans = 0;
+            for (int i = 1; i < numbers.Count - 1; i++)
+            {
+                if ((numbers[i] > numbers[i-1] && numbers[i] > numbers[i+1]) || 
+                    (numbers[i] < numbers[i - 1] && numbers[i] < numbers[i + 1]))
+                {
+                    ans++;
+                }
+            }
+            return ans;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Leetcode/Medium/1717. Maximum Score From Removing Substrings/1717. Maximum Score From Removing Substrings/Program.cs
@@ -0,0 +1,71 @@
+﻿namespace _1717._Maximum_Score_From_Removing_Substrings
+{
+    public class Solution
+    {
+        public int MaximumGain(string s, int x, int y)
+        {
+            int ans = 0;
+            string higher, lower;
+
+            // decide which pair is higher value
+            if (x > y)
+            {
+                higher = "ab";
+                lower = "ba";
+            }
+            else
+            {
+                higher = "ba";
+                lower = "ab";
+            }
+
+            // First pass: remove higher-value pairs
+            Stack<char> st = new Stack<char>();
+            foreach (char c in s)
+            {
+                if (st.Count > 0 && st.Peek() == higher[0] && c == higher[1])
+                {
+                    st.Pop();
+                    ans += Math.Max(x, y);
+                }
+                else
+                {
+                    st.Push(c);
+                }
+            }
+
+            // Build remaining string from stack (reverse order)
+            List<char> remainList = new List<char>();
+            while (st.Count > 0)
+                remainList.Add(st.Pop());
+            remainList.Reverse();
+            string remain = new string(remainList.ToArray());
+
+            // Second pass: remove lower-value pairs
+            st = new Stack<char>();
+            foreach (char c in remain)
+            {
+                if (st.Count > 0 && st.Peek() == lower[0] && c == lower[1])
+                {
+                    st.Pop();
+                    ans += Math.Min(x, y);
+                }
+                else
+                {
+                    st.Push(c);
+                }
+            }
+
+            return ans;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.MaximumGain("aabbrtababbabmaaaeaabeawmvaataabnaabbaaaybbbaabbabbbjpjaabbtabbxaaavsmmnblbbabaeuasvababjbbabbabbasxbbtgbrbbajeabbbfbarbagha",8484,4096));
+        }
+    }
+}
