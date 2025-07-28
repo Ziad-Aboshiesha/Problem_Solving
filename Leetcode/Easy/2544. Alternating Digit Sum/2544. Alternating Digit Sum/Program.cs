98e76f3bc5db1cd7e520f0627ac30729abb29c6a
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-07-28 18:14:12 +0300

    2544

---
+++ Leetcode/Easy/2544. Alternating Digit Sum/2544. Alternating Digit Sum/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _2544._Alternating_Digit_Sum
+{
+    public class Solution
+    {
+        public int AlternateDigitSum(int n)
+        {
+            int ans = 0;
+            List<int> list = new List<int>();
+            while(n!= 0 )
+            {
+                list.Add(n%10);
+                n = n/10;
+            }
+            int sign = 1;
+            if (list.Count % 2 == 0) 
+            {
+                sign = -1;
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                ans += sign * list[i];
+                sign *= -1;
+            }
+            return ans;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
