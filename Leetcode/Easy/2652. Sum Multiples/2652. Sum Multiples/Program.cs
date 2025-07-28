1258f932c1de4d55605aa3468f2bf0fe64f41c61
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-07-28 08:52:01 +0300

    2652

---
+++ Leetcode/Easy/2652. Sum Multiples/2652. Sum Multiples/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _2652._Sum_Multiples
+{
+
+    public class Solution
+    {
+        public int SumOfMultiples(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 || i % 7 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
