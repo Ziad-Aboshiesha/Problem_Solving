8ba9fd876336241ef2b8209a3058a4332a17bf14
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2025-07-28 18:44:06 +0300

    1470

---
+++ Leetcode/Easy/1470. Shuffle the Array/1470. Shuffle the Array/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1470._Shuffle_the_Array
+{
+    public class Solution
+    {
+        public int[] Shuffle(int[] nums, int n)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < n; i+=1) 
+            {
+                result.Add(nums[i]);
+                result.Add(nums[i+n]);
+            }
+            return result.ToArray();
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int[] nums = s.Shuffle([2, 5, 1, 3, 4, 7], 3);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+    }
+}
