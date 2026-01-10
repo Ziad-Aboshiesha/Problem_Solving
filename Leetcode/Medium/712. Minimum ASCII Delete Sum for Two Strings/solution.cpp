e5713fb08a281c94422343af6104429c1addb3cc
Ziad-Aboshiesha <ziadaboshiesha3@gmail.com>
2026-01-10 10:08:33 +0200

    712

---
+++ Leetcode/Medium/712. Minimum ASCII Delete Sum for Two Strings/solution.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        int m = s1.size();
+        int n = s2.size();
+        vector<vector<int>> dp (m+1, vector<int>(n+1,0));
+        int as1 = 0;
+        
+        for(int i =0 ; i < m ; i++)
+        {
+            as1+=(int) s1[i];
+        }
+        for(int i =0 ; i < n ; i++)
+        {
+            as1+= (int) s2[i];
+        }
+        for(int i =1; i <= m; i++)
+        {
+            for(int j =1 ; j <= n ; j++)
+            {
+                if(s1[i-1] == s2[j-1])
+                {
+                    dp[i][j] = (int)s1[i-1]+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        int ans = as1  - (2* dp[m][n]);
+        return ans;
+
+    }
+};
